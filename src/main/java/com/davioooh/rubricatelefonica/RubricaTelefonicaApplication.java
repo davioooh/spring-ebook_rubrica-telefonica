c563182f79515b1eb4b080696830d5c633663052
David Castelletti <castellettid@gmail.com>
2021-03-08 10:45:06 +0100

    Setup iniziale progetto

---
+++ src/main/java/com/davioooh/rubricatelefonica/RubricaTelefonicaApplication.java
@@ -0,0 +1,13 @@
+package com.davioooh.rubricatelefonica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubricaTelefonicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RubricaTelefonicaApplication.class, args);
+    }
+
+}
