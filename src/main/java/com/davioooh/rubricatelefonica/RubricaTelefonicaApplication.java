f7f79ed07af106427efb0a34048619817cc4795e
David Castelletti <castellettid@gmail.com>
2021-03-15 10:03:45 +0100

    Riformatta codice

--- src/main/java/com/davioooh/rubricatelefonica/RubricaTelefonicaApplication.java
+++ src/main/java/com/davioooh/rubricatelefonica/RubricaTelefonicaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RubricaTelefonicaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RubricaTelefonicaApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(RubricaTelefonicaApplication.class, args);
+  }
 
 }
